febd58216d0b2318bd6d2e9a21aa2583c34603ba
yashkirr <me@yashkir.co.za>
2021-12-03 19:31:57 +0200

    update proc

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import flask
 
-app = flask.Flask("__main__")
+app = flask.Flask(__name__)
 
 @app.route('/')
 def home():
     return flask.render_template("index.html")
 
-app.run(debug=False)
+if __name__ == '__main__':
+    app.run(debug=False)
